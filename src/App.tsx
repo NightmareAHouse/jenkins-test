24b4a7247e11b46a07fb2e5112ce5b0fd5ad861e
NightmareAHouse <55136609+nightmareahouse@users.noreply.github.com>
2022-06-20 15:23:49 +0300

    change text

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Jenkinsjthtdrbgrs
+          Learn React and Jenkins
         </a>
       </header>
     </div>
